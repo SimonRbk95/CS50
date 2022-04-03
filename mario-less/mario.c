c3e4559fe85a914605fcfbad41c4586b9687d2e6
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-03 15:07:09 +0000

    Sun, Apr 3, 2022, 3:07 PM +00:00

---
+++ mario-less/mario.c
@@ -0,0 +1,34 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void){
+
+    //mention function declared below
+    void createPyramide(int height);
+
+    // prompt for input as long as condition is met
+    int height;
+    do{
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    // create building with n hashtags
+    createPyramide(height);
+}
+
+void createPyramide(int height){
+    for (int i = 1; i < height; i++){
+
+            // for each column print '.'
+            for (int l = height - i; l >= 1; l--){
+                printf(" ");
+            }
+            // for each column print '#'
+            for(int j = 1; j <= i; j++){
+                printf("#");
+            }
+        // move to next row
+            printf("\n");
+        }
+}
