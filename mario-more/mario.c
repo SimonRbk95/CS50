9b20a7e1acd6feb10d8e699fdf2fe79b233ed9e2
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-03 09:37:54 +0000

    Sun, Apr 3, 2022, 9:37 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -16,7 +16,7 @@ int main(void)
     for (int i = 0; i <= n; i++)
     {
         // for each column
-        for(int x = 0; x <= i; x++)
+        for(int j = 0; x <= j; j++)
         {
             printf("#");
         }
@@ -27,7 +27,7 @@ int main(void)
 for (int i = 0; i <= n; i++)
     {
         // for each column
-        for(int x = 0; x <= i; x++)
+        for(int j = 0; x <= j; j++)
         {
             printf("#");
         }
