b1cbcbdfd65f604e83dbd94f83c3d743d9fe9e3f
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-03 15:23:06 +0000

    Sun, Apr 3, 2022, 3:23 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -8,7 +8,9 @@ int main(void)
 
     // prompt for input as long as condition is met
     int height;
-    do{
+
+    do
+    {
         height = get_int("Height: ");
     }
     while (height < 1 || height > 8);
@@ -16,22 +18,26 @@ int main(void)
     // create building with n hashtags
     createPyramide(height);
 }
-void createPyramide(int height){
-    for (int i = 1; i <= height; i++){
-
-            // for each column print space
-            for (int l = height - i; l >= 1; l--){
+void createPyramide(int height)
+    {
+    for (int i = 1; i <= height; i++)
+    {
+        // for each column print space
+        for(int l = height - i; l >= 1; l--)
+            {
                 printf(" ");
             }
             // for each column print '#'
-            for(int j = 1; j <= i; j++){
+            for(int j = 1; j <= i; j++)
+            {
                 printf("#");
             }
             // for each row print double space
             printf("  ");
 
             // for each column print mirrored '#'
-            for(int k = 1; k <= i; k++){
+            for(int k = 1; k <= i; k++)
+            {
                 printf("#");
             }
 
