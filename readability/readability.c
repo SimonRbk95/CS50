29ddd54eb815fc2bdd435f6cc5696902360f1d9d
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-08 15:28:15 +0000

    Fri, Apr 8, 2022, 3:28 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -80,10 +80,10 @@ int count_sentences(string text)
 
 int grade_calculator(letters, words, sentences)
 {
-    float L = letters/words * 100;
-    float S = sentences/words * 100;
-    float index = 0.0588 * L - 0.296 * S - 15.8;
-    float round(float index);
+    double L = float letters/words * 100;
+    double S = sentences/words * 100;
+    double index = 0.0588 * L - 0.296 * S - 15.8;
+    double round(float index);
     return index;
 
 }
