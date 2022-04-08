d51ea1011389fd2cb610e183f872a030e3e97624
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-08 06:59:41 +0000

    Fri, Apr 8, 2022, 6:59 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -6,7 +6,7 @@
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 // Array of letters of the alphabet
-int CHARS[] = {A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z};
+string CHARS[] = {A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z};
 
 int compute_score(string word);
 
