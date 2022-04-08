8c6eb2921b1bbfcd5ebfec626bc1d786c0a59278
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-08 11:41:02 +0000

    Fri, Apr 8, 2022, 11:41 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,8 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int count_letters(string text);
+
 int main(void)
 {
-string text = getstring("Text: \n");
+    string text = getstring("Text: \n");
+    printf("%s", text);
+    printf(count_letter(text));
 
+}
+
+int count_letters(string text)
+{
+    // 
+    // return Grade
 }
