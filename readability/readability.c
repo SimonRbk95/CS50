eb08887331356be89155bcf5588b8b45408dd9f9
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-08 15:57:07 +0000

    Fri, Apr 8, 2022, 3:57 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -95,7 +95,7 @@ int grade_calculator(letters, words, sentences)
     double S = (float)sentences / (float)words * 100;
     double index = 0.0588 * L - 0.296 * S - 15.8;
     printf("index before round: %f", index);
-    float round(float index);
+    double  round(float index);
     printf("index after round: %f", index);
     return index;
 }
