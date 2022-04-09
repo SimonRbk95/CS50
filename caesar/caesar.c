581bb2c2c7f7912173863fbbb8bea6447a66f7b0
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-09 11:32:10 +0000

    Sat, Apr 9, 2022, 11:32 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -3,12 +3,22 @@
 
 int main(int argc, string argv[])
 {
+    // 1 if there is no parameter or more than one return 1 with an error message
+    if(argv[1] == nul])
+    {
+        return 1;
+    }    // 2 check if the argument's characters are digits
     string digits = "123456789";
     printf("This the key's first index: %c", argv[1][1]);
+    // check
+    for ()
+    {
+
+    }
 
     //must take one parameter
-    // 1 if there is no parameter or more than one return 1 with an error message
-    // 2 check if the argument's characters are digits
+
+
     // 3 if not step 2
     // implement the encryption equation
         // lower & upper case letters must remain lower and upper, respectively
