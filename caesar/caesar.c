c1bd3d498e5c9b4e08b8f67ab91549eec2eb36f4
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-09 17:57:50 +0000

    Sat, Apr 9, 2022, 5:57 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -8,7 +8,29 @@ string encrypt_message( string key);
 
 int main(int argc, string argv[])
 {
-    valid_input(argv[1]);
+    if (argc != 2)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+    int key = argv[1];
+    int index = 0;
+    // check if it is a digit
+    while(index < strlen(key))
+    {
+            // cast parameter into integer to get ascii
+            if ((int) key[index] >= 48 && (int) key[index] <= 57)
+            {
+                // check next digit
+                index += 1;
+            }
+            // if it is not a digit exit out
+            else
+            {
+                printf("Usage: ./caesar key\n");
+                return 1;
+            }
+    }
     string cipher = encrypt_message(argv[1]);
     printf("Ciphertext: %s\n", cipher);
 }
