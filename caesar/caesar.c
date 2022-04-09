c229608fe7a2f1a6d7b44542330917ea503cb3d7
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-09 11:03:37 +0000

    Sat, Apr 9, 2022, 11:03 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -7,7 +7,9 @@ int main(int argc, string argv[])
     // 1 if there is no parameter or more than one return 1 with an error message
     // 2 check if the argument's characters are digits
     // 3 if not step 2
-    // 
+    // implement the encryption equation
+    // ask for input
+    // output the text using the encryption eqution
     int a = (int) argv[1] + 2;
 
 
