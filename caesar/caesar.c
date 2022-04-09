d3e0e55f1aac8cc2b7e2bbfb6e4ee548c32aaf4b
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-09 10:59:58 +0000

    Sat, Apr 9, 2022, 10:59 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -3,8 +3,9 @@
 
 int main(int argc, string argv[])
 {
-    printf("This is your parameter: %s\n", argv[1]);
+    //must take one parameter
+    // if there is no parameter
     int a = (int) argv[1] + 2;
-    printf("%s + 2\n", argv[1]);
+
 
 }
