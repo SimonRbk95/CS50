6dde3588fd6f569ef03e00f8af31ae8492863724
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-09 17:51:26 +0000

    Sat, Apr 9, 2022, 5:51 PM +00:00

--- test.c
+++ test.c
@@ -3,15 +3,134 @@
 #include <string.h>
 #include <stdlib.h>
 
+int valid_input(string key);
+string encrypt_message( string key);
 
-int main(void)
+int main(int argc, string argv[])
 {
-    char upper[] = {'A', 'B', '!'};
-    int ascii = upper[0];
-    (printf("ascii: %i\n", ascii));
-    char Anew = ascii + 1;
-    (printf("new ascii: %c\n", Anew));
-    upper[0] = Anew;
-    (printf("new array: %s\n", upper));
+    if (argc != 2)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+    int key = argv[1]
+    int index = 0;
+    // check if
+    if(!key)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+    // check if it is a digit
+    while(index < strlen(key))
+    {
+            // cast parameter into integer to get ascii
+            if ((int) key[index] >= 48 && (int) key[index] <= 57)
+            {
+                // check next digit
+                index += 1;
+            }
+            // if it is not a digit exit out
+            else
+            {
+                printf("Usage: ./caesar key\n");
+                return 1;
+            }
+    }
+    return 0;
+    string cipher = encrypt_message(argv[1]);
+    printf("Ciphertext: %s\n", cipher);
 }
 
+string encrypt_message(string key)
+{
+    // turn key into an integer
+    int k = atoi(key);
+    // prompt for user's message
+    string plaintext = get_string("Plaintext:  ");
+    // replace each char of plaintext
+    for(int index = 0; index < strlen(plaintext); index++)
+    {
+        // get the ascii value of each character in plaintext
+        int l = plaintext[index];
+        // check if l is uppercase
+        if (l >= 65 && l <= 90)
+        {
+            // rotate letter by: (p + key)%26
+            int rotation_value = (l + k) % 26;
+            // keep track of alphabet starting at A
+            int count = l - 65;
+            // increase the count to rotate through the alphabet
+            for (int n = rotation_value; n > 0; n--)
+                if(count < 25)
+                {
+                    count++;
+
+                }
+                else
+                {
+                    count = 0;
+                }
+            // update the ascii value of rotated letter
+            l = count + 65;
+            // convert new ascii value to letter
+            char cipher = l;
+            // replace the current letter in plaintext
+            plaintext[index] = cipher;
+        }
+
+        // check if l is lowercase
+        else if (l >= 97 && l <= 122)
+        {
+            // rotate letter by: (p + key)%26
+            int rotation_value = (l + k) % 26;
+            // keep track of alphabet starting at A
+            int count = l - 65;
+            // increase the count to rotate through the alphabet
+            for (int n = rotation_value; n > 0; n--)
+                if(count < 25)
+                {
+                    count++;
+                }
+                else
+                {
+                    count = 0;
+                }
+            // update the ascii value of rotated letter
+            l = count + 65;
+            // convert new ascii value to letter
+            char cipher = l;
+            // replace the current letter in plaintext
+            plaintext[index] = cipher;
+        }
+    }
+return plaintext;
+}
+
+int valid_input(string key)
+{
+    int index = 0;
+    // check if
+    if(!key)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+    // check if it is a digit
+    while(index < strlen(key))
+    {
+            // cast parameter into integer to get ascii
+            if ((int) key[index] >= 48 && (int) key[index] <= 57)
+            {
+                // check next digit
+                index += 1;
+            }
+            // if it is not a digit exit out
+            else
+            {
+                printf("Usage: ./caesar key\n");
+                return 1;
+            }
+    }
+    return 0;
+}
