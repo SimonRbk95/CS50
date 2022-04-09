94d8efc1f01d8901a230bb2841f3460420ce2b14
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-09 12:20:44 +0000

    Sat, Apr 9, 2022, 12:20 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -6,15 +6,28 @@ int valid_input(string key);
 
 int main(int argc, string argv[])
 {
-    printf("First Digit: %c\n",argv[1][0]);
-    printf("Second Digit: %c\n",argv[1][1]);
-    printf("key = %s", argv[1]);
-    printf("First ascii convert: %i\n", (int) argv[1][0]);
-    printf("Secon ascii convert: %i\n", (int) argv[1][1]);
     valid_input(argv[1]);
 }
 
-    // 1 if there is no parameter or more than one return 1 with an error message
+
+
+    // implement the encryption equation
+        // lower & upper case letters must remain lower and upper, respectively
+        // loop through the alphabet based on the input #.
+
+    // ask for input
+    // output the text using the encryption eqution
+        // after outputting return 0 to exit main
+
+
+
+
+
+
+
+
+
+
 
 int valid_input(string key)
 {
@@ -31,10 +44,12 @@ int valid_input(string key)
             // cast parameter into integer to get ascii
             if ((int) key[index] >= 48 && (int) key[index] <= 57)
             {
-                index += 1;
                 // test
                 printf("ascii: %i\n",key[index]);
+                // check next digit
+                index += 1;
             }
+            // if it is not a digit exit out
             else
             {
                 printf("Usage: ./caesar key\n");
@@ -46,24 +61,7 @@ int valid_input(string key)
 
 
 
-    // 2 check if the argument's characters are digits
 
-    // printf("This the key's first index: %c", argv[1][1]);
-    // check
-
-
-
-    //must take one parameter
-
-
-    // 3 if not step 2
-    // implement the encryption equation
-        // lower & upper case letters must remain lower and upper, respectively
-        // loop through the alphabet based on the input #.
-
-    // ask for input
-    // output the text using the encryption eqution
-        // after outputting return 0 to exit main
 
 
 
