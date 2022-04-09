a7885a688d9f0ea5074646924b77d6b20ac70c12
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-09 11:18:54 +0000

    Sat, Apr 9, 2022, 11:18 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -3,13 +3,14 @@
 
 int main(int argc, string argv[])
 {
+    char digits = []
     //must take one parameter
     // 1 if there is no parameter or more than one return 1 with an error message
     // 2 check if the argument's characters are digits
     // 3 if not step 2
     // implement the encryption equation
         // lower & upper case letters must remain lower and upper, respectively
-        // loop through the alphabet based on the input #. 
+        // loop through the alphabet based on the input #.
 
     // ask for input
     // output the text using the encryption eqution
