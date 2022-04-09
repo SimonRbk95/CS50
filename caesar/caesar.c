d23a059ee9f6c0f4d245b52ecb8f98cbca336f97
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-09 11:54:20 +0000

    Sat, Apr 9, 2022, 11:54 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,40 +1,39 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
-    string digits = "123456789";
     int index = 0;
-
-    while(index < strlen(argv[1]))
-    {
-            // get ASCII value
-            (int) argv[1][index];
-            // check if it is a digit
-            if (argv[1][index] == i)
-            {
-                index += 1;
-            }
-        }
-    }
-
-    // 1 if there is no parameter or more than one return 1 with an error message
     if(! argv[1])
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
-
-
-
-
-    else if ()
+    while(index < strlen(argv[1]))
     {
+            // get ASCII value
+            int ascii = argv[1][index];
+            // check if it is a digit
+            if (argv[1][index] >= 48  argv[1][index] <= 57)
+            {
+                index += 1;
+                printf("ascii: %i",argv[1][index]);
+            }
+        }
+    }
+
+    // 1 if there is no parameter or more than one return 1 with an error message
+
+
+
+
+
     // 2 check if the argument's characters are digits
 
-    printf("This the key's first index: %c", argv[1][1]);
+    // printf("This the key's first index: %c", argv[1][1]);
     // check
-    }
+
 
 
     //must take one parameter
