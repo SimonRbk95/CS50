c1cc231630fa8abb4b812cec886218a531ce5a71
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-09 11:27:22 +0000

    Sat, Apr 9, 2022, 11:27 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -4,10 +4,10 @@
 int main(int argc, string argv[])
 {
     char digits[] = "123456789";
-    char key[] = (int) argv[1];
+    char key[] = argv[1];
 
     printf("This the key's first index: %c", key[1]);
-    
+
     //must take one parameter
     // 1 if there is no parameter or more than one return 1 with an error message
     // 2 check if the argument's characters are digits
