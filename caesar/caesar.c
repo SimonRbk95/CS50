67391d6597384f85d2cb3d400fc0f1c5422ff6d1
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-10 08:32:57 +0000

    Sun, Apr 10, 2022, 8:32 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -61,7 +61,7 @@ int valid_input(int argc, string key)
     if (argc != 2)
     {
         printf("Usage: ./caesar key\n");
-        exit(0);
+        exit(1);
     }
     // string key = argv[1];
     int index = 0;
@@ -78,7 +78,7 @@ int valid_input(int argc, string key)
             else
             {
                 printf("Usage: ./caesar key\n");
-                exit(0);
+                exit(1);
             }
     }
     return 0;
