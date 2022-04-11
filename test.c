ee9db8dc512597b363566d1eadd38e424e2d7467
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-11 18:53:24 +0000

    Mon, Apr 11, 2022, 6:53 PM +00:00

--- test.c
+++ test.c
@@ -3,27 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-int valid_input(string key);
-    if (argc != 2)
-    {
-        printf("Usage: ./caesar key\n");
-        return 1;
-    }
-    string key = argv[1];
-    int index = 0;
-    // check if it is a digit
-    while(index < strlen(key))
-    {
-            // cast parameter into integer to get ascii
-            if ((int) key[index] >= 48 && (int) key[index] <= 57)
-            {
-                // check next digit
-                index += 1;
-            }
-            // if it is not a digit exit out
-            else
-            {
-                printf("Usage: ./caesar key\n");
-                return 1;
-            }
-    }
+int main(void)
+{
+    
+}
