63988adaed919555c506c3a605a31e4a61292ddb
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-12 18:45:11 +0000

    Tue, Apr 12, 2022, 6:45 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -90,11 +90,11 @@ void print_winner(void)
         // sort the array lowest to highest
         // bubble sort
             // set swap counter to non-zero value
-            int swap_counter = 1;
+            int swap_counter = -1;
             while (swap_counter > 0)
             {
                 swap_counter = 0;
-                
+
             }
             // repeat until the swap counter is 0:
                 // reset swap counter to 0
