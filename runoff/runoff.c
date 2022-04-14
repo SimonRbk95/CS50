003d2b7c26ba046f1fc206b9d32125858893cd69
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-14 10:42:52 +0000

    Thu, Apr 14, 2022, 10:42 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -180,7 +180,7 @@ int find_min(void)
 {
     // search for lowest vote
     // only consider non-eliminated candidates
-    int min;
+    int min_vote = 0;
     for (int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].eliminated == false)
