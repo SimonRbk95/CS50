7b4bd27350172dcb774479cb6ae087c75a364857
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-14 07:21:13 +0000

    Thu, Apr 14, 2022, 7:21 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -132,7 +132,7 @@ bool vote(int voter, int rank, string name)
     for (int i = 0; i < candidate_count; i++)
     {
         // look for a match between voters and voter
-        if (candidates[i].name == c)
+        if (strcmp(candidates[i].name, name) == 0)
         {
 
         }
