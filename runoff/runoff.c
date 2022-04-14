bc6e0a6bd65ea143965f4396b1ea7e4b6d666376
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-14 08:30:21 +0000

    Thu, Apr 14, 2022, 8:30 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max voters and candidates
 #define MAX_VOTERS 100
@@ -134,7 +135,7 @@ bool vote(int voter, int rank, string name)
         // look for a match between voters and voter
         if (strcmp(candidates[i].name, name) == 0)
         {
-            preferences[voter][rank] = name;
+            preferences[voter][rank] name;
             return true;
         }
     }
