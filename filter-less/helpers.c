2fadc3af103e90127662e4fb7388962e95b58a9d
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-17 10:27:01 +0000

    Sun, Apr 17, 2022, 10:27 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -8,18 +8,20 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for(j = 0; j < width; j++)
             {
+                // float variables for easier calculation and rounding precision
                 float b = image[i][j].rgbtBlue;
                 float r = image[i][j].rgbtRed;
                 float g = image[i][j].rgbtGreen;
+                // get the adequate shade of grey
                 int avg = round((b + r + g)/3);
+                // 
                 if (avg > 255)
                 {
                     avg = 255;
                 }
-                
-
-
-
+                image[i][j].rgbtBlue = avg;
+                image[i][j].rgbtRed = avg;
+                image[i][j].rgbtGreen = avg;
             }
     }
     return;
