27022a1e0eda41b828b2fc13cf4c4b33831aed12
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-17 20:05:59 +0000

    Sun, Apr 17, 2022, 8:05 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -1,6 +1,7 @@
 #include "helpers.h"
 #include "math.h"
 #include "cs50.h"
+#include "stdio.h"
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
