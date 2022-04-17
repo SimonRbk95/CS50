f3b0815c2202ba4020d8529b991ec8d25dd9ea92
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-17 19:39:25 +0000

    Sun, Apr 17, 2022, 7:39 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -31,7 +31,27 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+            {
+                // float variables for easier calculation and rounding precision
+                float b, r, g;
+                b = image[i][j].rgbtBlue;
+                r = image[i][j].rgbtRed;
+                g = image[i][j].rgbtGreen;
+                // get the adequate shade of grey
+                int avg = round((b + r + g)/3);
+                if (avg > 255)
+                {
+                    avg = 255;
+                }
+                // turn the pixel grey
+                image[i][j].rgbtBlue = avg;
+                image[i][j].rgbtRed = avg;
+                image[i][j].rgbtGreen = avg;
+            }
+    }
     return;
 }
 
