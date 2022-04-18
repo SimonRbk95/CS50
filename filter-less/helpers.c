0f114a0f3653b2ff3e2c45a0d4b08788583cad99
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-18 11:29:40 +0000

    Mon, Apr 18, 2022, 11:29 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -88,14 +88,15 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
                 // if the number of pixels in a row is even
                 if (width % 2 == 0)
                 {
+                    swap(tempRed, tempBlue, tempGreen, i, j);
                     // start from the last pixel and swap them with the counterparts starting from the beginning
-                    image[i][j].rgbtBlue = image[i][width - j - 1].rgbtBlue;
-                    image[i][j].rgbtRed = image[i][width - j - 1].rgbtRed;
-                    image[i][j].rgbtGreen = image[i][width - j - 1].rgbtGreen;
+                    //image[i][j].rgbtBlue = image[i][width - j - 1].rgbtBlue;
+                    //image[i][j].rgbtRed = image[i][width - j - 1].rgbtRed;
+                    //image[i][j].rgbtGreen = image[i][width - j - 1].rgbtGreen;
 
-                    image[i][width - j - 1].rgbtRed = tempRed;
-                    image[i][width - j - 1].rgbtGreen = tempGreen;
-                    image[i][width - j - 1].rgbtBlue = tempBlue;
+                    //image[i][width - j - 1].rgbtRed = tempRed;
+                    //image[i][width - j - 1].rgbtGreen = tempGreen;
+                    //image[i][width - j - 1].rgbtBlue = tempBlue;
                 }
                 // in case width is odd
                 else
@@ -103,13 +104,14 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
                     // skip the pixel in the middle
                     if (j != (width + 1) / 2)
                     {
-                    image[i][j].rgbtBlue = image[i][width - j - 1].rgbtBlue;
-                    image[i][j].rgbtRed = image[i][width - j - 1].rgbtRed;
-                    image[i][j].rgbtGreen = image[i][width - j - 1].rgbtGreen;
+                    swap(tempRed, tempBlue, tempGreen, i, j)
+                    //image[i][j].rgbtBlue = image[i][width - j - 1].rgbtBlue;
+                    //image[i][j].rgbtRed = image[i][width - j - 1].rgbtRed;
+                    //image[i][j].rgbtGreen = image[i][width - j - 1].rgbtGreen;
 
-                    image[i][width - j - 1].rgbtRed = tempRed;
-                    image[i][width - j - 1].rgbtGreen = tempGreen;
-                    image[i][width - j - 1].rgbtBlue = tempBlue;
+                    //image[i][width - j - 1].rgbtRed = tempRed;
+                    //image[i][width - j - 1].rgbtGreen = tempGreen;
+                    //image[i][width - j - 1].rgbtBlue = tempBlue;
                     }
                 }
             }
