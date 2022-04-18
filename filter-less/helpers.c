1ecb444ac6e659533bbd4bda9976c8eb3a651978
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-18 11:05:39 +0000

    Mon, Apr 18, 2022, 11:05 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -105,7 +105,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
                 // in case width is odd
                 else
                 {
-                    if (j != (width -1)/2)
+                    if (j != (width + 1) / 2)
                 }
             }
     }
