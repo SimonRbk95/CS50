1d9a959e6a51f01164398bb921bb56cbc549f510
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-19 12:40:00 +0000

    Tue, Apr 19, 2022, 12:40 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -21,7 +21,10 @@ while (fread(buffer, 1, block_size, raw_file) == block_size)
         && buffer[2] == 0xff
         && (buffer[3] & 0xf0) == 0xe0)
     {
-
+        // make a new JPEG and keep track of it
+        sprintf(filename, "%03i.jpg", 2);
+        FILE *img = fopen(filename, "w");
+        fwrite(data, );
     }
     // If first JPEG
         // make a new JPEG and open it
