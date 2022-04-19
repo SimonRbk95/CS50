3ff0e01cd0200fa217294b0e1c4ce5634e507210
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-19 11:45:06 +0000

    Tue, Apr 19, 2022, 11:45 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 int main(int argc, char *argv[])
 {
@@ -7,20 +8,25 @@ int main(int argc, char *argv[])
 // new type to store a byte of data
 typedef uint8_t BYTE;
 int block_size = BYTE * 512
+// pointer to dynamic memory for blocks to be read
+*buffer = malloc(block_size);
  // open memory card
-    FILE *file = fopen(argv[1], "r");
- // repeat until end of card:
-    // Read 512 byte into a buffer
-    *buffer = malloc(block_size);
-    // If start of new JPEG
-        // If first JPEG
-            // make a new JPEG and open it
-            // f(write) to the new file
-        // Else
-            // close the file I've been already writing to
+FILE *file = fopen(argv[1], "r");
+// repeat until end of block:
+while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
+{
+// If start of new JPEG
+    // If first JPEG
+        // make a new JPEG and open it
+        // f(write) to the new file
     // Else
-        // If already found JPEG
-            // keep writing to it
+        // close the file I've been already writing to
+// Else
+    // If already found JPEG
+        // keep writing to it
+}
+
+
 // close any remaining files
 
 }
