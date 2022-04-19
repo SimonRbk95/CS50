11d1ce99334c039c9a6f45b1fb46a13ed595751b
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-19 11:03:32 +0000

    Tue, Apr 19, 2022, 11:03 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+ // read data from the memory card
+ // make a new JPEG and open it
+ // f(write) to the new file
+
 }
