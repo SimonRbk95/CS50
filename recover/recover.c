de636d18e74021fc34b4bce89cb4a099f1e59df8
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-19 11:05:44 +0000

    Tue, Apr 19, 2022, 11:05 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,6 +3,13 @@
 
 int main(int argc, char *argv[])
 {
+ // open memory card
+ // repeat until end of card:
+    // Read 512 byte into a buffer
+    // If start of new JPEG
+        // If first JPEG
+            //...
+        // Else
  // read data from the memory card
  // make a new JPEG and open it
  // f(write) to the new file
