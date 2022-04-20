2014e19ad0d4fd503f6c520f1eddebae1b1e7cb1
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-20 07:04:49 +0000

    Wed, Apr 20, 2022, 7:04 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -5,57 +5,57 @@
 int main(int argc, char *argv[])
 {
 
-if (argc != 2)
-{
-    printf("Usage: ./recover Filename");
-    return 1;
-}
-
-// new type to store a byte of data
-typedef uint8_t BYTE;
-
-// pointer to dynamic memory for one block
-BYTE *buffer = malloc(512);
-
-// open memory card
-FILE *raw_file = fopen(argv[1], "r");
-
-if(raw_file == NULL)
-{
-    printf("File cannot be opened!");
-    return 1;
-}
-
-// write to
-FILE *img = NULL;
-
-// keep track of files created
-int JPEG_COUNTER = 0;
-
-char filename[8];
-
-// repeat until end of block:
-while (fread(buffer, sizeof(BYTE), 512, raw_file))
-{
-    // If start of new JPEG
-    if (buffer[0] == 0xff
-        && buffer[1] == 0xd8
-        && buffer[2] == 0xff
-        && (buffer[3] & 0xf0) == 0xe0)
+    if (argc != 2)
     {
-        // make a new JPEG and keep track of it
-        sprintf(filename, "%03i.jpg", JPEG_COUNTER);
-        // open the file with write privileges
-        img = fopen(filename, "w");
-        JPEG_COUNTER++;
+        printf("Usage: ./recover Filename");
+        return 1;
     }
-    // prevent programm from trying to write if input is invalid, i.e. not the start of the first image
-    if (img != NULL)
+
+    // new type to store a byte of data
+    typedef uint8_t BYTE;
+
+    // pointer to dynamic memory for one block
+    BYTE *buffer = malloc(512);
+
+    // open memory card
+    FILE *raw_file = fopen(argv[1], "r");
+
+    if (raw_file == NULL)
     {
-        fwrite(buffer, 512, 1, img);
+        printf("File cannot be opened!");
+        return 1;
     }
-}
-free(buffer);
-fclose(img);
-fclose(raw_file);
+
+    // write to
+    FILE *img = NULL;
+
+    // keep track of files created
+    int JPEG_COUNTER = 0;
+
+    char filename[8];
+
+    // repeat until end of block:
+    while (fread(buffer, sizeof(BYTE), 512, raw_file))
+    {
+        // If start of new JPEG
+        if (buffer[0] == 0xff
+            && buffer[1] == 0xd8
+            && buffer[2] == 0xff
+            && (buffer[3] & 0xf0) == 0xe0)
+        {
+            // make a new JPEG and keep track of it
+            sprintf(filename, "%03i.jpg", JPEG_COUNTER);
+            // open the file with write privileges
+            img = fopen(filename, "w");
+            JPEG_COUNTER++;
+        }
+        // prevent programm from trying to write if input is invalid, i.e. not the start of the first image
+        if (img != NULL)
+        {
+            fwrite(buffer, 512, 1, img);
+        }
+    }
+    free(buffer);
+    fclose(img);
+    fclose(raw_file);
 }
