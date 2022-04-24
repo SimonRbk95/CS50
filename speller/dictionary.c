0817935f0667ea09e7dc8be13eb4fc130427de53
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-24 09:26:39 +0000

    Sun, Apr 24, 2022, 9:26 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -103,7 +103,7 @@ bool unload(void)
     for (int i = 0; i < N; i++)
     {
         node* cursor = table[i];
-        while (table[i] != NULL)
+        while (cursor != NULL)
         {
             node *tmp = cursor;
             cursor = cursor->next;
