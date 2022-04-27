f470ff40f5e8ddb525ddbe0d4f35ad9f2b2d99a7
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-27 18:15:47 +0000

    Wed, Apr 27, 2022, 6:15 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,15 +1,23 @@
 
+def main():
+    height = valid_input()
+    make_pyramide(heigh)
+
+
 def valid_input():
     while True:
         height = int(input("Height: "))
         if height <= 8:
             return height
 
-height = valid_input()
-for i in range(height):
-    print(" " * (height-i), end="")
-    print("#" * (i + 1))
 
+def make_pyramide(height):
+    for i in range(height):
+        print(" " * (height-i), end="")
+        print("#" * (i + 1), end="")
+        print(" ", end="")
+        print("#" * (i + 1), end="")
+        print(" " * (height-i))
 
-
-
+if __name__ == "__main__:
+    main()
