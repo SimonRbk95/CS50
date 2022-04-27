dd79529a127a729e34049546b7bcac7daa3f359c
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-27 14:26:30 +0000

    Wed, Apr 27, 2022, 2:26 PM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -60,7 +60,7 @@ def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
     while len(teams) > 1:
-        simulate_round(teams)
+        teams = simulate_round(teams)
     return teams[0]["team"]
 
 
