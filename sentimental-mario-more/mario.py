2f1094ac51b459d0c031008b0b82a8f8c1efc2a6
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-27 18:01:58 +0000

    Wed, Apr 27, 2022, 6:01 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,5 +1,9 @@
-height = int(input("Height: "))
+
+def valid_input():
+    while True:
+        height = int(input("Height: "))
+        if height <= 8:
+            return height
 
 for i in range(height):
     for j in range(height):
-        
