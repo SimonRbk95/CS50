d5fde37bf3bf22a2adcb14ccba6052acd494dfe6
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-28 19:51:40 +0000

    Thu, Apr 28, 2022, 7:51 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -35,7 +35,7 @@ def count_sentences(text):
     for words in text:
         for word in words:
             for char in word:
-                if ord(char) != 33 or ord(char) != 63 or ord(char) != 46:
+                if ord(char) != 33 and ord(char) != 63 and ord(char) != 46:
                     count +=1
     return count
 
