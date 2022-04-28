58ed8beca85e001bee1b93d6d85df73b53c3944f
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-28 07:40:12 +0000

    Thu, Apr 28, 2022, 7:40 AM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,6 +1,40 @@
 from cs50 import get_float
 
 def main():
-    dollars = cs50.get_float("Change owed: ")
-    
+    dollars = get_float("Change owed: ")
+    # turn dollars into cents
+    dollars *=100
+
+def calculate_quarters(dollars):
+    quarters = 0
+    while dollars>=25:
+        dollars -= 25
+        quarters += 1
+    return quarters
+
+
+def calculate_dimes(dollars):
+    dimes = 0
+    while dollars>=10:
+        dollars -= 10
+        dimes += 1
+    return dimes
+
+def calculate_nickels(dollars):
+    nickels = 0
+    while dollars>=10:
+        dollars -= 10
+        nickels += 1
+    return nickels
+
+def calculate_pennies(dollars):
+    pennies = 0
+    while dollars>=1:
+        dollars -= 1
+        pennies += 1
+    return pennies
+
+
+
+
 
