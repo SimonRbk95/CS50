592dce89575a0c47d806ffa34f7d1c679333fd70
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-28 19:03:03 +0000

    Thu, Apr 28, 2022, 7:03 PM +00:00

---
+++ sentimental-readability/readability.py
@@ -0,0 +1,19 @@
+def main():
+    text = input("Text: ")
+    pass
+
+
+def count_letters():
+    pass
+
+
+def count_words():
+    pass
+
+
+def count_sentences():
+    pass
+
+
+def grade_calculator():
+    pass
