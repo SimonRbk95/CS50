3a114d47e6c54202309149ffb7774bcbab1bffb9
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-04-28 19:17:16 +0000

    Thu, Apr 28, 2022, 7:17 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,4 +1,5 @@
 import string
+import sys
 
 def main():
     text = input("Text: ")
@@ -7,14 +8,17 @@ def main():
 
 def count_letters():
     # count character
+    pass
 
 
 def count_words(text):
     word_count = len(text.split())
+    print(word_count)
     return word_count
 
 
-def count_sentences():
+def count_sentences(text):
+    count_one = len(text.split())
     pass
 
 
