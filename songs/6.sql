06a83e668e1317bfcfb94381fd5078be01e3aeda
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-01 12:19:07 +0000

    Sun, May 1, 2022, 12:19 PM +00:00

--- songs/6.sql
+++ songs/6.sql
@@ -1,1 +1,2 @@
-SELECT songs.name FROM songs INNER JOIN artists ON artists.id = songs.artist_id WHERE artists.id = songs.artist_id;
+SELECT songs.name FROM songs WHERE artists.name = "Post Malone";
+SELECT songs.name FROM songs INNER JOIN artists ON artists.id = songs.artist_id WHERE artists.name = "Post Malone";
