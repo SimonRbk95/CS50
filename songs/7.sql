5bf23478f4274c51a0c50f90c469af8f943d64d8
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-02 12:15:50 +0000

    Mon, May 2, 2022, 12:15 PM +00:00

--- songs/7.sql
+++ songs/7.sql
@@ -1,1 +1,2 @@
 SELECT AVG(energy) FROM songs INNER JOIN artists ON artists.id = songs.artist_id WHERE artists.name = "Drake";
+SELECT AVG(energy) FROM songs WHERE artist_id = (SELECT id FROM artists WHERE name = 'Drake');
--- songs/8.sql
+++ songs/8.sql
@@ -1,1 +1,2 @@
 SELECT name from songs WHERE name REGEXP 'feat?';
+SELECT name from songs WHERE name LIKE '%feat.%;
