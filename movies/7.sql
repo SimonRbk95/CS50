b702f6b3d0b607f970856e2e9661a5790d74d3a2
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-03 09:06:10 +0000

    Tue, May 3, 2022, 9:06 AM +00:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,2 +1,2 @@
 SELECT title, year FROM movies WHERE year = 2010 ORDER BY (SELECT )
-SELECT movies.title FROM movies INNER JOIN ratings ON ratings.movie_id = movies.id ORDER BY (SELECT DISTINCT rating FROM ratings);
+SELECT movies.title, ratings.rating FROM movies INNER JOIN ratings ON ratings.movie_id = movies.id ORDER BY (SELECT DISTINCT rating FROM ratings);
