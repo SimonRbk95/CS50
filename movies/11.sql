ba48e62483a0f92b8892054f5024bccb0e4a70fd
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-04 17:23:58 +0000

    Wed, May 4, 2022, 5:23 PM +00:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,1 +1,2 @@
-SELECT title FROM movies JOIN ratings ON movies.id = ratings.movie_id JOIN stars ON movies.id = stars.movie_id WHERE stars.
+SELECT title FROM movies JOIN ratings ON movies.id = ratings.movie_id JOIN stars ON movies.id = stars.movie_id WHERE
+SELECT title FROM movies WHERE 
