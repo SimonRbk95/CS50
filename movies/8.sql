a09dd583d3eedbd08ff62772ac216fa12b6ec2f8
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-04 19:54:28 +0000

    Wed, May 4, 2022, 7:54 PM +00:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,2 +1,1 @@
 SELECT name from people WHERE id IN (SELECT person_id FROM stars WHERE movie_id IN (SELECT id FROM movies WHERE title = "Toy Story"));
-/* SELECT people.name FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON movies.id = stars.movie_id WHERE movies.title = "Toy Story"; 
