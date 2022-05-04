6b4b4b576c9258fbb5b1acd36a07f4ddbcd44af8
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-04 18:56:36 +0000

    Wed, May 4, 2022, 6:56 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,1 +1,1 @@
-SELECT name FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON movies.id = stars.movies_id WHERE name IN (SELECT )
+SELECT name FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON movies.id = stars.movies_id WHERE name IN (SELECT name )
