6c60fb25e32795cf800c909d83a5045c27d20b5a
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-04 17:17:38 +0000

    Wed, May 4, 2022, 5:17 PM +00:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,1 +1,1 @@
-SELECT name FROM people WHERE id IN (SELECT person_id FROM stars WHERE movie_id IN (SELECT movie_id FROM ratings WHERE rating >= 9.0));
+SELECT DISTINCT name FROM people WHERE id IN (SELECT person_id FROM directors WHERE movie_id IN (SELECT movie_id FROM ratings WHERE rating >= 9.0));
