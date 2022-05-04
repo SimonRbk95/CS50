b71d60f1bcba128fc6cae326f2d99ea37eb8b885
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-04 17:35:42 +0000

    Wed, May 4, 2022, 5:35 PM +00:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,4 +1,4 @@
-SELECT title FROM movies JOIN ratings ON movies.id = ratings.movie_id JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id WHERE people.id = "Chadwick Bosemann" AND (SELECT TOP 5 FROM ratings.rating);
+SELECT TOP 5 title FROM movies JOIN ratings ON movies.id = ratings.movie_id JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id WHERE people.id = "Chadwick Bosemann" ORDER BY ratings.rating;
 
 WHERE people.name = "Chadwick Bosemann"
 SELECT title FROM movies WHERE name IN (SELECT people )
