5e72d874cd2f92fcc9359d692423ca6639902828
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-04 19:14:03 +0000

    Wed, May 4, 2022, 7:14 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,4 +1,7 @@
 SELECT name FROM people
 JOIN stars ON people.id = stars.person_id JOIN movies ON movies.id = stars.movie_id
 WHERE movies.id IN
+(SELECT )
+
+
 (SELECT id FROM movies WHERE people.name = "Kevin Bacon" AND people.birth = 1958);
