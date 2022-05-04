ed76cc3919fd5311877ebbd0711f52c665863377
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-04 17:52:37 +0000

    Wed, May 4, 2022, 5:52 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,5 +1,1 @@
-SELECT titles FROM movies
-JOiN stars
-ON movies.id = stars.movie_id
-JOIN people
-ON people.id = stars.person_id
+SELECT titles FROM movies JOiN stars ON movies.id = stars.movie_id JOIN people ON people.id = stars.person_id WHERE "Johnny Depp" AND "Helena Bonha Carter" IN people;
