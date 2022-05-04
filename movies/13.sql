2a037f940982b4af8562051449a18397fc1ced39
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-04 19:18:26 +0000

    Wed, May 4, 2022, 7:18 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,3 +1,1 @@
-SELECT name FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON movies.id = stars.movie_id
-WHERE movies.id IN
-(SELECT id FROM movies JOIN stars ON people.id = stars.person_id JOIN movies ON movies.id = stars.movie_id WHERE people.name = "Kevin Bacon" AND people.birth = 1958) AND people.name != "Kevin Bacon";
+SELECT name FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON movies.id = stars.movie_id WHERE movies.id IN (SELECT id FROM movies JOIN stars ON people.id = stars.person_id JOIN movies ON movies.id = stars.movie_id WHERE people.name = "Kevin Bacon" AND people.birth = 1958) AND people.name != "Kevin Bacon";
