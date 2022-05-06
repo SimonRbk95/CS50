b3c5d36516f28f6e67b0ba98104ac6e498ee82a3
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-06 19:00:04 +0000

    Fri, May 6, 2022, 7:00 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -2,3 +2,8 @@
 
 -- check all descriptions
 SELECT description FROM crime_scene reports
+
+-- get descriptions of instances where stuff got stolen
+SELECT description
+FROM crime_scene_reports
+WHERE description IN ("%theft%", "%burglary%, %Shoplifiting)
