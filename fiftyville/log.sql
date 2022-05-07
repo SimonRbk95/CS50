959caba12f6e2d017487563da2fb0c7aa63d6135
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-07 11:48:08 +0000

    Sat, May 7, 2022, 11:48 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -76,4 +76,4 @@ ON bank_accounts.person_id = people.id
 WHERE account_number = (SELECT account_number
                         FROM atm_transactions
                         WHERE year = 2021 AND month = 07 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw");
-
+-- LUCA
