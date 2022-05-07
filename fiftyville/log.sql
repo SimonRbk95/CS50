b376f33c2e12f3074d058654e3598e536dc7d400
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-07 09:33:30 +0000

    Sat, May 7, 2022, 9:33 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -34,11 +34,16 @@ ORDER BY minute;
 SELECT account_number
 FROM atm_transactions
 WHERE year = 2021 AND month = 07 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw";
--
+-- gives account number of the people who withdrew money
 
 -- 3rd lead: on the phone, the thief talked about taking the earliest flight out of Fiftyville on the next day (07/29/21)
 -- thief asked the other guy on the phone to purchase the tickets
--- get passport#
+-- we have:
+-- 1. airport name
+-- 2. the day of the booking
+-- 3. the day of the phone call
+-- 4. the person on the receiving end of the call booked the tickets
+
 SELECT passport_number
 FROM passengers
 JOIN flights
