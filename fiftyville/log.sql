c818c98ed362c7486a9dc48d020ca312bb4c5ba8
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-07 11:20:00 +0000

    Sat, May 7, 2022, 11:20 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,6 +3,8 @@
 -- check all descriptions
 SELECT description FROM crime_scene reports;
 
+
+
 -- get descriptions of instances where stuff got stolen
 -- examining cases with keyword "theft" first
 SELECT description, year, month, day
@@ -11,11 +13,15 @@ WHERE description LIKE"%Theft%"
 -- Found theft of CS50 duck
 -- 28th of July at 10:15am at Humphrey Street bakery, three witnesses, each interview metions bakery
 
+
+
 -- check interviews
 SELECT transcript
 FROM interviews
 WHERE transcript LIKE "%bakery%";
 -- each statement gives great insights, need to pursue each one of them
+
+
 
 -- 1st lead: car that left within 10 minutes of the theft.
 -- Look at parking lot security footage in that time frame
@@ -28,12 +34,16 @@ AND minute BETWEEN 15 AND 20
 ORDER BY minute;
 -- names of owners ban be looked up based upon license plate numbers
 
+
+
 -- 2nd lead: earlier in the morning of the same day, the thief was seen at an ATM on Legett Street withdrawing
 -- try to find possibe suspect's transaction and his account number
 SELECT account_number
 FROM atm_transactions
 WHERE year = 2021 AND month = 07 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw";
 -- gives account number of the people who withdrew money. names can be looked up
+
+
 
 -- 3rd lead: on the phone, the thief talked about taking the earliest flight out of Fiftyville on the next day (07/29/21)
 -- thief asked the other guy on the phone to purchase the tickets
@@ -42,9 +52,9 @@ WHERE year = 2021 AND month = 07 AND day = 28 AND atm_location = "Leggett Street
 -- 2. the day of the booking
 -- 3. the day of the phone call
 -- 4. the person on the receiving end of the call booked the tickets
+-- match phone number, license plate number, passport_number to get right name
 
-
-
+-- get passport number of people that f
 SELECT passport_number
 FROM passengers
 JOIN flights
@@ -53,9 +63,6 @@ JOIN airports
 ON flights.origin_airport_id = airports.id
 WHERE airports.name = "Fiftyville"
 AND flights.year = 2021 AND flights.month = 7 AND flights.day = 29;
-
--- match phone number, license plate number, passport_number to get right name
-
 
 -- destinations that correspond to flights that depart from Fiftyville
 SELECT full_name
