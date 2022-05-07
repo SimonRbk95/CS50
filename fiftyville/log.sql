ef0d29c626da17bcfdbe82eaf39163b7cf121e13
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-07 13:36:44 +0000

    Sat, May 7, 2022, 1:36 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,15 +1,20 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- check all descriptions
-SELECT description FROM crime_scene reports;
-
+SELECT
+       description
+FROM
+       crime_scene reports;
 
 
 -- get descriptions of instances where stuff got stolen
 -- examining cases with keyword "theft" first
-SELECT description, year, month, day
-FROM crime_scene_reports
-WHERE description LIKE"%Theft%"
+SELECT
+       description, year, month, day
+FROM
+       crime_scene_reports
+WHERE
+       description LIKE "%Theft%";
 -- Found theft of CS50 duck
 -- 28th of July at 10:15am at Humphrey Street bakery, three witnesses, each interview metions bakery
 
--- fiftyville/test.sql
+++ fiftyville/test.sql
@@ -1,1 +1,19 @@
-SELECT name FROM people WHERE phone_number IN (Select caller FROM phone_calls WHERE year = 2021 AND month = 07 AND day = 28) ORDER BY name;
+-- Keep a log of any SQL queries you execute as you solve the mystery.
+
+-- check all descriptions
+SELECT
+       description
+FROM
+       crime_scene reports;
+
+
+-- get descriptions of instances where stuff got stolen
+-- examining cases with keyword "theft" first
+SELECT
+       description, year, month, day
+FROM
+       crime_scene_reports
+WHERE
+       description LIKE "%Theft%";
+-- Found theft of CS50 duck
+-- 28th of July at 10:15am at Humphrey Street bakery, three witnesses, each interview metions bakery
