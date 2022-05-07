89ddf237775aedfc48d6648258e46dce49a9bf8f
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-07 14:07:24 +0000

    Sat, May 7, 2022, 2:07 PM +00:00

--- fiftyville/test.sql
+++ fiftyville/test.sql
@@ -118,3 +118,21 @@ WHERE
             LIMIT
                 1
     )
+
+SELECT
+    name
+FROM
+    people
+WHERE
+    phone_number
+IN
+    (
+    Select
+        caller
+    FROM
+        phone_calls
+    WHERE
+        year = 2021
+        AND month = 07
+        AND day = 28
+    )
