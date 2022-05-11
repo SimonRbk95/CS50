647337ab68d6a1efd832dcd36ff1a53b7b2e3363
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-11 20:25:32 +0000

    Wed, May 11, 2022, 8:25 PM +00:00

--- trivia/script.js
+++ trivia/script.js
@@ -1,47 +1,7 @@
-
-let btn1 = document.getElementById("Answer1");
-let btn2 = document.getElementById("Answer2");
-let btn3 = document.getElementById("Answer3");
-let btn4 = document.getElementById("Answer4");
-
-
 document.addEventListener("DOMContentLoaded", function (){
     // When correct answer is clicked
     let correct = document.getElementById("Answer3");
     correct.addEventListener("click", function (){
         correct.style.backgroundColor = "green";
-        
+        document.getElemenById("placeholder1").innerText = "Correct!";
     }
-    ]
-})
-
-while (true)
-{
-    checkAnswer(btn1);
-    checkAnswer(btn2);
-    checkAnswer(btn3);
-    checkAnswer(btn4);
-}
-
-
-
-
-
-
-function checkAnswer(btnX)
-{
-    btnX.addEventListener("click", function onClick()
-    {
-        if (btnX.innerText === "World Wide Web")
-        {
-            // change color of box to green
-            btnX.style.backgroundColor = "green";
-
-        }
-        else
-        {
-            // change color of box to red
-            btnX.style.backgroundColor = "red";
-        }
-    });
-}
