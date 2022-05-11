cbc24481fc6f1cb137059ed81025c4187166f1aa
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-11 08:05:42 +0000

    Wed, May 11, 2022, 8:05 AM +00:00

--- trivia/script.js
+++ trivia/script.js
@@ -1,5 +1,6 @@
-for (let i= 0; i<3; i++){
-    document.getElementById("Answer" + str(i)).addEventListener("click", function {
-        if 
+for (let i= 0; i<3; i++)
+{
+    document.getElementById("Answer" + str(i)).addEventListener("click", function() {
+        if (document.getElementById("Answer" + str(i)) === "World Wide Web"
     });
 }
