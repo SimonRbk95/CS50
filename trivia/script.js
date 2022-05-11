36b285b5f0e8462cf9a93a145e987929e2715901
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-11 08:35:24 +0000

    Wed, May 11, 2022, 8:35 AM +00:00

--- trivia/script.js
+++ trivia/script.js
@@ -3,7 +3,11 @@ for (let i= 0; i<3; i++)
     document.getElementById("Answer" + string(i)).addEventListener("click", function() {
         if (document.getElementById("Answer" + string(i)) === "World Wide Web")
         {
-            
+            // change color of box to green
+        }
+        else
+        {
+            // change color of box to red
         }
     }
     );
