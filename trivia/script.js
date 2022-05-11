7a388bd169540401cb7dac97b9980340b6fb1305
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-11 20:27:17 +0000

    Wed, May 11, 2022, 8:27 PM +00:00

--- trivia/script.js
+++ trivia/script.js
@@ -1,7 +1,7 @@
-document.addEventListener("DOMContentLoaded", function (){
+document.addEventListener("DOMContentLoaded", function(){
     // When correct answer is clicked
     let correct = document.getElementById("Answer3");
-    correct.addEventListener("click", function (){
+    correct.addEventListener("click", function(){
         correct.style.backgroundColor = "green";
         document.getElemenById("placeholder1").innerText = "Correct!";
     }
