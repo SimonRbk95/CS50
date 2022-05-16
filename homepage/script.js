2b341b7416fca6e995e2ef71af4aa57a06b25aa1
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-16 20:07:41 +0000

    Mon, May 16, 2022, 8:07 PM +00:00

---
+++ homepage/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+    // When button is clicked
+    let btn = document.querySelectorAll("");
+    correct.addEventListener("click", function(){
+        correct.style.backgroundColor = "green";
+        document.getElementById("placeholder1").innerHTML = "Correct!";
+    });
