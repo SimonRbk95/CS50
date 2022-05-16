643fa9e72e8260e60a25fbb5fd9ad3bd28d91f38
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-16 20:11:11 +0000

    Mon, May 16, 2022, 8:11 PM +00:00

--- homepage/script.js
+++ homepage/script.js
@@ -1,10 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(){
     // When button is clicked
-    let buttons = document.querySelectorAll(".button");
-    for (let i = 0; i < incorrects.length; i++){
-        incorrects[i].addEventListener("click", function(){
-            incorrects[i].style.backgroundColor = "red";
-            document.getElementById("placeholder1").innerHTML = "incorrect!";
+    let buttons = document.querySelectorAll(".btn-outline-light");
+    for (let i = 0; i < buttons.length; i++){
+        buttons[i].addEventListener("click", function(){
+            
             });
         }
     }
