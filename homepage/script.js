b529512dd62a2863c7b31998aaa22814c6bcf019
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-19 11:13:35 +0000

    Thu, May 19, 2022, 11:13 AM +00:00

--- homepage/script.js
+++ homepage/script.js
@@ -8,15 +8,3 @@ document.addEventListener("DOMContentLoaded", function(){
         }
     });
 
-// asynchronously load the images before the carousel tries itsel
-let img = new Image(),
-url = "background_1.1.png",
-container = document.getElementById("img-holder-1");
-img.onload = function () { container.appendChild(img); };
-img.src = url;
-
-let img_2 = new Image(),
-url_2 = "background_3.1.png",
-container_2 = document.getElementById("img-holder-2");
-img_2.onload = function () { container_2.appendChild(img_2); };
-img_2.src = url_2;
