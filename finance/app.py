7e38f34a9d2c78d6a50dc40a799bb9543465eab1
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-24 14:01:32 +0000

    Tue, May 24, 2022, 2:01 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -128,7 +128,7 @@ def register():
             return apology("The username already exists", 403)
         # check if password matches the requirements
         elif valid_password(password, password_repeat):
-            db.execute()
+            db.execute("INSERT INTO users (username,, day) VALUES (?, ?, ?);", name, month, day)
 
         # elif password does not match the repeat password field
         # else submit the user's input via POST to /register
