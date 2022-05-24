b484c57bab3965d90daf3e4d626f0af5ecf18be6
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-24 16:23:36 +0000

    Tue, May 24, 2022, 4:23 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -151,8 +151,29 @@ def register():
         if db.execute("SELECT username FROM users WHERE username = %s", (username, )):
             # appropriate apology message
             return apology("The username already exists", 403)
-        # check if password matches the requirements
-        elif valid_password(password, password_repeat):
+
+            # check if password matches the requirements
+        pCounter = 0
+        dCounter = 0
+        # require that the password be at least 8 characters long
+        if len(password) < 8:
+            return apology("The password is too short", 403)
+        for char in password:
+            print(char)
+            if char in string.punctuation:
+                pCounter += 1
+            elif char.isdigit():
+                dCounter += 1
+        # require that the password includes at least 2 digits and 1 special character
+        if not (pCounter >= 1 and dCounter >= 2):
+            print(dCounter)
+            return apology("Your password does not contain at least 2 digits and 1 special characters", 403)
+        # check if the password has been repeated correctly
+        elif password != password_repeat:
+            print("no match")
+            return apology("Your passwords don't match", 403)
+        # if password checks out
+        else:
             print("validated")
             db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, generate_password_hash(password))
             return redirect("/login")
