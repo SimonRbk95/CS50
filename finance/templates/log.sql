13fcba295fc1ad35ce29d6f082b54b786b4f71e8
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-26 11:39:21 +0000

    Thu, May 26, 2022, 11:39 AM +00:00

---
+++ finance/templates/log.sql
@@ -0,0 +1,12 @@
+CREATE TABLE portfolio(
+   id INTEGER PRIMARY KEY,
+   user_id INTEGER,
+   symbol TEXT NOT NULL,
+   quantity INTEGER NOT NULL,
+   purchase_price INTEGER NOT NULL,
+   pruchase_date INTEGER NOT NULL,
+   FOREIGN KEY (user_id)
+      REFERENCES users (id)
+         ON DELETE CASCADE
+         ON UPDATE NO ACTION
+);
