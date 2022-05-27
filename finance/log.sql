9af1a9d8b94abb9a2a077d3ce62fcac307737cd0
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-27 11:52:08 +0000

    Fri, May 27, 2022, 11:52 AM +00:00

--- finance/log.sql
+++ finance/log.sql
@@ -5,6 +5,7 @@ CREATE TABLE portfolio(
    quantity INTEGER NOT NULL,
    purchase_price INTEGER NOT NULL,
    sale_price INTEGER NOT NULL DEFAULT 0,
+   purchase BOOLEAN NOT NULL CHECK (purchase IN (0,1)),
    Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
    FOREIGN KEY (user_id)
       REFERENCES users (id)
