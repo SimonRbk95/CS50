657416e7bafcc78efe1f9cd428506d9736f9edb1
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-27 19:57:20 +0000

    Fri, May 27, 2022, 7:57 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -107,7 +107,7 @@ def buy():
             # adjust user's budget
             db.execute("UPDATE users SET cash = cash - (?) WHERE id = (?)",
                         purchase, session["user_id"])
-        return redirect(url_for("index"))
+        return redirect("/")
     else:
         return render_template("buy.html")
 
