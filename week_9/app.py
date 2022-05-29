be4202b1902d1e78d5103614f6af96bdbba60e42
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-29 13:24:51 +0000

    Sun, May 29, 2022, 1:24 PM +00:00

--- week_9/app.py
+++ week_9/app.py
@@ -1,10 +1,16 @@
 import os
 import string
 
+from flask_session import Session
 from flask import Flask, render_template, request
 from cs50 import SQL
 
 app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 
 @app.route("/")
 
