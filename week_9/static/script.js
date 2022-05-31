a35970184014e950716eb47765cbb10ff4a1a203
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-05-31 12:32:05 +0000

    Tue, May 31, 2022, 12:32 PM +00:00

---
+++ week_9/static/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){
+    // When button is clicked
+    let buttons = document.querySelectorAll(".btn-outline-light");
+    for (let i = 0; i < buttons.length; i++){
+        buttons[i].addEventListener("click", function(){
+            window.location.href = '/q1';
+            });
+        }
+    });
+
