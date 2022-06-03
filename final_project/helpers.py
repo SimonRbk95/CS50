e636a3ccbf495a70ff2ce3fe562372f9eced93be
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-06-03 18:04:25 +0000

    Fri, Jun 3, 2022, 6:04 PM +00:00

--- final_project/helpers.py
+++ final_project/helpers.py
@@ -6,8 +6,8 @@ from flask import redirect, render_template, request, session
 
 def read_csv(csv_file):
     contents = []
-    with open(csv_file) as file:
-        reader = csv.DictReader(file)
+    with open(csv_file, new_line= "") as file:
+        reader = csv.DictReader(file, delimiter="\t")
         for row in reader:
             contents.append(row)
         return contents
