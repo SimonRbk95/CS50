2aa5bf216625c9adead94c65aecafe574d01e77a
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-06-03 12:12:02 +0000

    Fri, Jun 3, 2022, 12:12 PM +00:00

--- final_project/helpers.py
+++ final_project/helpers.py
@@ -1,5 +1,17 @@
 import requests
 import urllib.parse
+import csv
 
 from flask import redirect, render_template, request, session
 from functools import wraps
+
+def read_file():
+    coursera_100 = []
+    with open("") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            coursera_100.append(row)
+        return coursera_100
+
+def read_txt():
+    
