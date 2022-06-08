e726b26aa457189e61c40ef327208ab571495fdb
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-06-08 10:10:41 +0000

    Wed, Jun 8, 2022, 10:10 AM +00:00

--- final_project/static/script.js
+++ final_project/static/script.js
@@ -10,11 +10,7 @@ document.addEventListener("DOMContentLoaded", function(){
     elem.style.marginRight = 'auto';
     elem.style.marginLeft = 'auto';
 
-    document.querySelector('.select-field').addEventListener('click',()=>{
-        document.querySelector('.list').classList.toggle('show');
-        document.querySelector('.down-arrow').classList.toggle('rotate180');
 
-    });
 
 });
 
