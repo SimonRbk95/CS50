83736ecca7f5e9b29176aef9a987b64853623562
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-06-09 11:43:56 +0000

    Thu, Jun 9, 2022, 11:43 AM +00:00

--- final_project/static/script.js
+++ final_project/static/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
     // When button is clicked
-    let buttons = document.querySelectorAll(".btn-outline-light");
+    let buttons = document.querySelectorAll(".qs");
     for (let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener("click", function(){
             window.location.href = '/qs';
