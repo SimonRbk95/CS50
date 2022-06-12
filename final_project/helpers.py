d8c63b111ea93161613a1804720ecfa260badb01
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-06-12 11:26:26 +0000

    Sun, Jun 12, 2022, 11:26 AM +00:00

--- final_project/helpers.py
+++ final_project/helpers.py
@@ -12,5 +12,14 @@ def read_csv(csv_file):
             contents.append(dict(row))
         return contents
 
+def lookup(symbol):
+    """Look up quote for symbol."""
 
-
+    # Contact API
+    try:
+        api_key = os.environ.get("API_KEY")
+        url = f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}"
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.RequestException:
+        return None
