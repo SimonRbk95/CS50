393fb69eb967f0d57f79abe7588aa3fab998ffe2
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-06-12 12:12:02 +0000

    Sun, Jun 12, 2022, 12:12 PM +00:00

--- final_project/helpers.py
+++ final_project/helpers.py
@@ -7,16 +7,17 @@ import shutil
 from flask import redirect, render_template, request, session
 
 def read_gz():
-    if not exists()
-    with gzip.open('static/cdb.txt.gz', 'rb') as f_in:
-        with open('static/cdb.txt', 'wb') as f_out:
-            shutil.copyfileobj(f_in, f_out)
-        contents = []
-        with open('static/cdb.txt', newline= "") as file:
-            reader = csv.DictReader(file, delimiter="\t")
-            for row in reader:
-                contents.append(dict(row))
-            print(contents[0])
+    if not exists("static/cdb.txt"):
+        with gzip.open('static/cdb.txt.gz', 'rb') as f_in:
+            with open('static/cdb.txt', 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+            
+    contents = []
+    with open('static/cdb.txt', newline= "") as file:
+        reader = csv.DictReader(file, delimiter="\t")
+        for row in reader:
+            contents.append(dict(row))
+        print(contents[0])
 
 def lookup(course):
     """Look up quote for symbol."""
