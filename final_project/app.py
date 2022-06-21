6a5a5c0d360e9c233d5b4589a5401d8408515b61
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-06-21 21:41:39 +0000

    Tue, Jun 21, 2022, 9:41 PM +00:00

--- final_project/app.py
+++ final_project/app.py
@@ -74,14 +74,14 @@ def qs():
                     for i in ["Data Analytics", "Data Science"]:
                         if i in dict["Product Name"] or i in dict["Primary Domain"] or i in dict["Primary Subdomain"]:
                                 if "Get a new job" in List_q4:
-                                    if dict["Product Type"] == "Professional Certificate":
+                                    if dict["Product Type"] == "Professional Certificate" and len(courses) < 5:
                                         courses.append({
                                             "Course Name": dict["Product Name"],
                                             "URL": dict["URL"],
                                             "Partner": dict["Partner"],
                                             "Certificate" : dict["Product Type"],
                                             })
-                                else:
+                                elif len(courses) < 5:
                                     courses.append({
                                     "Course Name": dict["Product Name"],
                                     "URL": dict["URL"],
@@ -89,17 +89,17 @@ def qs():
                                     "Certificate": dict["Product Type"],
                                     })
 
-                elif choice in dict["Product Name"] or choice in dict["Primary Domain"] or choice in dict["Primary Subdomain"]:
+                elif choice in dict["Product Name"] or choice in dict["Primary Domain"] or choice in dict["Primary Subdomain"] and len(courses) < 5:
                     # only append courses with professional certificates if "get a new job" is the goal
                     if "Get a new job" in List_q4:
-                        if dict["Product Type"] == "Professional Certificate":
+                        if dict["Product Type"] == "Professional Certificate" and len(courses) < 5:
                             courses.append({
                                 "Course Name": dict["Product Name"],
                                 "URL": dict["URL"],
                                 "Partner": dict["Partner"],
                                 "Certificate": dict["Product Type"],
                                 })
-                    else:
+                    elif len(courses) < 5:
                         courses.append({
                         "Course Name": dict["Product Name"],
                         "URL": dict["URL"],
