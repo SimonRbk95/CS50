85c242d1e9ca9bacee56fa44b5fb2b2f8b19e6d6
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-06-22 13:30:21 +0000

    Wed, Jun 22, 2022, 1:30 PM +00:00

--- final_project/helpers.py
+++ final_project/helpers.py
@@ -55,13 +55,14 @@ def check_dict(choice, dict):
                     # only append courses with professional certificates if "get a new job" is the goal
                     if "Get a new job" in List_q4:
                         if dict["Product Type"] == "Professional Certificate" and len(courses) < 5:
-                            append_dict(dict, courses)
+                            courses = append_dict(dict, courses)
                     elif len(courses) < 5:
-                        append_dict(dict, courses)
+                        courses = append_dict(dict, courses)
     elif choice in dict["Product Name"] or choice in dict["Primary Domain"] or choice in dict["Primary Subdomain"] and len(courses) < 5:
         if "Get a new job" in List_q4:
             if dict["Product Type"] == "Professional Certificate" and len(courses) < 5:
-                append_dict(dict, courses)
+                courses = append_dict(dict, courses)
         elif len(courses) < 5:
+            courses = append_dict(dict, courses)
+    return courses
 
-    pass
