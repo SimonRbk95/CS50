3067e467e118a82b7a3a7ad2a1b2a00f7603b839
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-06-24 10:52:50 +0000

    Fri, Jun 24, 2022, 10:52 AM +00:00

--- final_project/helpers.py
+++ final_project/helpers.py
@@ -44,13 +44,14 @@ def lookup(course):
     except requests.RequestException:
         return None
 
-def append_dict(dict, courses, c=None):
+def append_dict(dict, courses, cdb100=None):
     courses.append({
     "Course Name": dict["Product Name"],
-    if c:
-        "URL": dict["Product URL"]
+    # different key for the cdb100 data base
+    if cdb100:
+        "URL": dict["URL"]
     else:
-         "URL": dict["URL"]
+         "URL": dict["Product URL"]
     "Partner": dict["Partner"],
     "Certificate": dict["Product Type"],
     })
@@ -63,13 +64,13 @@ def check_dict(courses, choice, dict, List_q4, max_courses):
                     # only append courses with professional certificates if "get a new job" is the goal
                     if "Get a new job" in List_q4:
                         if dict["Product Type"] == "Professional Certificate" and len(courses) < max_courses:
-                            courses = append_dict(dict, courses)
+                            courses = append_dict(dict, courses, False)
                     elif len(courses) < max_courses:
-                        courses = append_dict(dict, courses)
+                        courses = append_dict(dict, courses, False)
     elif choice in dict["Product Name"] or choice in dict["Primary Domain"] or choice in dict["Primary Subdomain"] and len(courses) < max_courses:
         if "Get a new job" in List_q4:
             if dict["Product Type"] == "Professional Certificate" and len(courses) < max_courses:
-                courses = append_dict(dict, courses)
+                courses = append_dict(dict, courses, False)
         elif len(courses) < max_courses:
-            courses = append_dict(dict, courses)
+            courses = append_dict(dict, courses, False)
     return courses
