77b3c40099e37ada7204224e4036cc4c6738bda6
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-06-24 10:57:23 +0000

    Fri, Jun 24, 2022, 10:57 AM +00:00

--- final_project/helpers.py
+++ final_project/helpers.py
@@ -44,34 +44,29 @@ def lookup(course):
     except requests.RequestException:
         return None
 
-def append_dict(dict, courses, cdb100=None):
+def append_dict(dict, courses):
     courses.append({
     "Course Name": dict["Product Name"],
-    # different key for the cdb100 data base
+    "URL": dict["URL"],
     "Partner": dict["Partner"],
     "Certificate": dict["Product Type"],
     })
-
-    if cdb100:
-        courses.append("URL": dict["URL"])
-    else:
-         "URL": dict["Product URL"]
     return courses
 
-def check_dict(courses, choice, dict, List_q4, max_courses):
+def check_dict(courses, choice, dict, List_q4, max_courses, cdb100=None):
     if choice == "Data Analytics/ Science":
         for i in ["Data Analytics", "Data Science"]:
             if i in dict["Product Name"] or i in dict["Primary Domain"] or i in dict["Primary Subdomain"] and len(courses) < max_courses:
                     # only append courses with professional certificates if "get a new job" is the goal
                     if "Get a new job" in List_q4:
                         if dict["Product Type"] == "Professional Certificate" and len(courses) < max_courses:
-                            courses = append_dict(dict, courses, False)
+                            courses = append_dict(dict, courses)
                     elif len(courses) < max_courses:
-                        courses = append_dict(dict, courses, False)
+                        courses = append_dict(dict, courses)
     elif choice in dict["Product Name"] or choice in dict["Primary Domain"] or choice in dict["Primary Subdomain"] and len(courses) < max_courses:
         if "Get a new job" in List_q4:
             if dict["Product Type"] == "Professional Certificate" and len(courses) < max_courses:
-                courses = append_dict(dict, courses, False)
+                courses = append_dict(dict, courses)
         elif len(courses) < max_courses:
-            courses = append_dict(dict, courses, False)
+            courses = append_dict(dict, courses)
     return courses
