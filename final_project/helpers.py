f54fd2278eff4099e653fbd31ed3b56ab662e649
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-06-25 12:26:51 +0000

    Sat, Jun 25, 2022, 12:26 PM +00:00

--- final_project/helpers.py
+++ final_project/helpers.py
@@ -97,15 +97,13 @@ def check_dict(courses, choice, dict, List_q4, max_courses, cdb100=None):
 
 def check_dict(courses, index, dict, List_q4, keywords_q3, max_courses, cdb100=None):
         if any(n in dict["Product Name"] for n in keywords_q3[index]):
-            if cdb100:
+            if cdb100 and len(courses) < max_courses:
                 # only append courses with professional certificates if "get a new job" is the goal
                 if "Get a new job" in List_q4:
                     if dict["Product Type"] == "Professional Certificate" and len(courses) < max_courses:
                         courses = append_dict_cdb100(dict, courses)
                 elif len(courses) < max_courses:
                     courses = append_dict_cdb100(dict, courses)
-            elif i in dict["Product Name"] and len(courses) < max_courses:
-                courses = append_dict_cdb(dict, courses)
         elif cdb100:
             if choice in dict["Product Name"] or choice in dict["Primary Domain"] or choice in dict["Primary Subdomain"] and len(courses) < max_courses:
                 if "Get a new job" in List_q4:
