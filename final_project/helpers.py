e60514ba75eb488d1191292a1c14e2279df848d5
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-06-25 11:34:40 +0000

    Sat, Jun 25, 2022, 11:34 AM +00:00

--- final_project/helpers.py
+++ final_project/helpers.py
@@ -66,35 +66,33 @@ def append_dict_cdb100(dict, courses):
     return courses
 
 def check_duplicates(courses, dict, cdb):
-        for course in courses:
-            if course["Course Name"] == dict["Product Name"]:
-                return True
+    for course in courses:
+        if course["Course Name"] == dict["Product Name"]:
+            return True
     return False
 
 
 
 def check_dict(courses, choice, dict, List_q4, max_courses, cdb100=None):
-            if choice == "Data Analytics/ Science":
-                for i in ["Data Analytics", "Data Science"]:
-                    if cdb100:
-                        if i in dict["Product Name"] or i in dict["Primary Domain"] or i in dict["Primary Subdomain"] and len(courses) < max_courses:
-                                # only append courses with professional certificates if "get a new job" is the goal
-                                if "Get a new job" in List_q4:
-                                    if dict["Product Type"] == "Professional Certificate" and len(courses) < max_courses:
-                                        courses = append_dict_cdb100(dict, courses)
-                                elif len(courses) < max_courses:
+        if choice == "Data Analytics/ Science":
+            for i in ["Data Analytics", "Data Science"]:
+                if cdb100:
+                    if i in dict["Product Name"] or i in dict["Primary Domain"] or i in dict["Primary Subdomain"] and len(courses) < max_courses:
+                            # only append courses with professional certificates if "get a new job" is the goal
+                            if "Get a new job" in List_q4:
+                                if dict["Product Type"] == "Professional Certificate" and len(courses) < max_courses:
                                     courses = append_dict_cdb100(dict, courses)
-                    elif i in dict["Product Name"] and len(courses) < max_courses:
-                        courses = append_dict_cdb(dict, courses)
-            elif cdb100:
-                if choice in dict["Product Name"] or choice in dict["Primary Domain"] or choice in dict["Primary Subdomain"] and len(courses) < max_courses:
-                    if "Get a new job" in List_q4:
-                        if dict["Product Type"] == "Professional Certificate" and len(courses) < max_courses:
-                            courses = append_dict_cdb100(dict, courses)
-                    elif len(courses) < max_courses:
-                        courses = append_dict_cdb100(dict, courses)
-            elif choice in dict["Product Name"] and len(courses) < max_courses:
+                            elif len(courses) < max_courses:
+                                courses = append_dict_cdb100(dict, courses)
+                elif i in dict["Product Name"] and len(courses) < max_courses:
                     courses = append_dict_cdb(dict, courses)
-            return courses
-        else:
-            return
+        elif cdb100:
+            if choice in dict["Product Name"] or choice in dict["Primary Domain"] or choice in dict["Primary Subdomain"] and len(courses) < max_courses:
+                if "Get a new job" in List_q4:
+                    if dict["Product Type"] == "Professional Certificate" and len(courses) < max_courses:
+                        courses = append_dict_cdb100(dict, courses)
+                elif len(courses) < max_courses:
+                    courses = append_dict_cdb100(dict, courses)
+        elif choice in dict["Product Name"] and len(courses) < max_courses:
+                courses = append_dict_cdb(dict, courses)
+        return courses
