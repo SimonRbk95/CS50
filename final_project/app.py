80b10835e4dff30ae6b01cf7f70ab87168de4fd4
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-06-26 15:34:51 +0000

    Sun, Jun 26, 2022, 3:34 PM +00:00

--- final_project/app.py
+++ final_project/app.py
@@ -72,7 +72,7 @@ def qs():
             # list of dictionaries with relevant course data
             courses=[]
             # check if there is a course in coursera's top 100 with a name smilair to the user's choice
-            check_all_courses(List_q4, courses, index, keywords_q3, max_courses, cdb100=cdb100)
+            check_all_courses(List_q4=List_q4, courses, index, keywords_q3, max_courses, cdb100=cdb100)
             # get further data for chosen courses from cdb
             for dict in cdb:
                 # keep track of at which index the list item, the dictionary called 'course', is
@@ -86,7 +86,7 @@ def qs():
                         # courses[i]["SKU"] = dict["Unique Merchant SKU"]
                     i+=1
             if len(courses) < max_courses:
-                courses = check_all_courses(courses, index, dict, keywords_q3, max_courses, cdb=cdb)
+                courses = check_all_courses(courses, index, keywords_q3, max_courses, cdb=cdb)
 
             # assign every dict a 'unique' identifier
             # (not worth it to check for duplicates - possibility of a match is extremely low)
