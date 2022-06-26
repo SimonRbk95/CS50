56d42c995f708de261318ef7db921d3c3711a9a5
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-06-26 09:37:31 +0000

    Sun, Jun 26, 2022, 9:37 AM +00:00

--- final_project/helpers.py
+++ final_project/helpers.py
@@ -72,17 +72,17 @@ def check_duplicates(courses, dict):
     return False
 
 def check_dict(courses, index, dict, List_q4, keywords_q3, max_courses, cdb100=None):
-        if cdb100:
-            if any(n in dict["Product Name"] or n in dict["Primary Domain"] or n in dict["Primary Subdomain"] for n in keywords_q3[int(index)]) :
-                if len(courses) < max_courses:
-                    # only append courses with professional certificates if "get a new job" is the goal
-                    if "Get a new job" in List_q4:
-                        if dict["Product Type"] == "Professional Certificate":
+        if len(courses) < max_courses:
+            if cdb100:
+                if any(n in dict["Product Name"] or n in dict["Primary Domain"] or n in dict["Primary Subdomain"] for n in keywords_q3[int(index)]) :
+                        # only append courses with professional certificates if "get a new job" is the goal
+                        if "Get a new job" in List_q4:
+                            if dict["Product Type"] == "Professional Certificate":
+                                courses = append_dict_cdb100(dict, courses)
+                        else:
                             courses = append_dict_cdb100(dict, courses)
-                    else:
-                        courses = append_dict_cdb100(dict, courses)
-        elif any(n in dict["Product Name"] for n in keywords_q3[int(index)]):
-                courses = append_dict_cdb(dict, courses)
+            elif any(n in dict["Product Name"] for n in keywords_q3[int(index)]):
+                    courses = append_dict_cdb(dict, courses)
         return courses
 
 
