88de4f63801ae393c843a075637a941c3bf2c66a
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-06-26 09:38:43 +0000

    Sun, Jun 26, 2022, 9:38 AM +00:00

--- final_project/helpers.py
+++ final_project/helpers.py
@@ -73,8 +73,7 @@ def check_duplicates(courses, dict):
 
 def check_dict(courses, index, dict, List_q4, keywords_q3, max_courses, cdb100=None):
         if len(courses) < max_courses:
-            if cdb100:
-                if any(n in dict["Product Name"] or n in dict["Primary Domain"] or n in dict["Primary Subdomain"] for n in keywords_q3[int(index)]) :
+            if cdb100 and any(n in dict["Product Name"] or n in dict["Primary Domain"] or n in dict["Primary Subdomain"] for n in keywords_q3[int(index)]) :
                         # only append courses with professional certificates if "get a new job" is the goal
                         if "Get a new job" in List_q4:
                             if dict["Product Type"] == "Professional Certificate":
