4baaa121eae46358bd5e4b8bffafbdda6f16c043
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-06-27 12:10:00 +0000

    Mon, Jun 27, 2022, 12:10 PM +00:00

--- final_project/helpers.py
+++ final_project/helpers.py
@@ -115,7 +115,7 @@ def YT_lookup(course):
     try:
         response = request.execute()
         print(response)
-    except Exception
+    except (KeyError, TypeError, ValueError):
         return None
 
 YT_lookup("Data Science")
