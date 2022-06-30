11652bd615096e3d8a6dce53c989de60cb438e26
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-06-30 19:06:39 +0000

    Thu, Jun 30, 2022, 7:06 PM +00:00

--- final_project/helpers.py
+++ final_project/helpers.py
@@ -128,7 +128,10 @@ def YT_lookup(course, maxResults):
                 # "thumbnail": response["items"][i]["snippet"]["thumbnails"]["medium"]["url"],
                 # "description": response["items"][i]["snippet"]["description"],
             })
+    print(YT_data)
     return YT_data
+
+YT_lookup("Data", 3)
 
 
 
