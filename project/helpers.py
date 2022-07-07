b161b579623731a199e2c9bac05d988d515abd9f
SimonRbk95 <94790659+SimonRbk95@users.noreply.github.com>
2022-07-07 21:18:38 +0000

    Thu, Jul 7, 2022, 9:18 PM +00:00

--- project/helpers.py
+++ project/helpers.py
@@ -92,7 +92,7 @@ def check_all_courses(courses, index, keywords_q3, max_courses, List_q4=None, cd
                                 courses = append_dict_cdb100(dict, courses, cdb)
                 for dict in cdb100:
                     if condition_coursera(dict, courses, keywords_q3, index, max_courses, True):
-                        courses = append_dict_cdb100(dict, courses, cdbP)
+                        courses = append_dict_cdb100(dict, courses, cdb)
             # in big data base search only for matches in product name
             # match is checked in app.py
             else:
